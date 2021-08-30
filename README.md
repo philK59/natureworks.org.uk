b9d7127b7e8e4cabd4a201b817241218cf1bffce
Jake Rayson <contact@growdigital.org>
2021-08-30 19:44:27 +0100

    switch to new site

--- README.md
+++
@@ -1,3 +0,0 @@
-# Nature Works
-
-[Hugo](https://gohugo.io/) static website for [Nature Works](https://www.natureworks.org.uk/) wildlife & forest garden design
--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: {
-    'postcss-css-variables': { },
-    autoprefixer: { browsers: ['last 2 versions', 'iOS >= 8'] }
-  },
-}
-
